b5f9c8dd5ca7121123f5dfcce10ecce272872b26
Naldo Tumundo <naldo_j3@yahoo.com>
2019-10-30 01:53:22 -0700

    add mysql data

---
+++ db/schema.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE burgers_db; 
+USE burgers_db; 
+
+CREATE TABLE burgers (
+	id INT(11) NOT NULL AUTO_INCREMENT, 
+	burger_name VARCHAR(100) NOT NULL, 
+	devoured BOOLEAN DEFAULT false, 
+	time_created DATETIME NOT NULL,
+	PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+INSERT INTO burgers (burger_name) VALUES ("Triple Patty Cheeseburger"); 
+INSERT INTO burgers (burger_name) VALUES ("BBQ Brisket Burger"); 
+INSERT INTO burgers (burger_name) VALUES ("Vegan Burger"); 
+INSERT INTO burgers (burger_name) VALUES ("Salmon Burger"); 
+INSERT INTO burgers (burger_name) VALUES ("Classic Dick's Burger"); 
+INSERT INTO burgers (burger_name) VALUES ("Soutwestern Burger"); 
+INSERT INTO burgers (burger_name) VALUES ("Louisiana Crispy Chicken Burger"); 
+INSERT INTO burgers (burger_name) VALUES ("Teriyaki Burger"); 
+INSERT INTO burgers (burger_name) VALUES ("Porky Pork Burger");
